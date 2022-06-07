4bed125a3c0d14de89c708ba8e8107946119ed8c
Danny <danny@position.exchange>
2022-06-07 10:40:04 +0700

    add dummy test

---
+++ assignment_test.go
@@ -0,0 +1,24 @@
+package harmony
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPreStakingEnabledCommittee(T *testing.T) {
+	shardNum := 2
+	shardSize := 4
+	shardHarmonyNodes := 2
+	for i := 0; i < shardNum; i++ {
+		fmt.Printf("Shard %d\n", i)
+		for j := 0; j < shardHarmonyNodes; j++ {
+			index := i + j*shardNum // The initial account to use for genesis nodes
+			fmt.Printf("hmy account index %d\n", index)
+		}
+		// add FN runner's key
+		for j := shardHarmonyNodes; j < shardSize; j++ {
+			index := i + (j-shardHarmonyNodes)*shardNum
+			fmt.Printf("fn account index %d\n", index)
+		}
+	}
+}
