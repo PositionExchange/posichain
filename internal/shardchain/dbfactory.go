f18998c2375b478c9aa670559bb8e710adcd5919
Danny <danny@position.exchange>
2022-07-27 16:40:00 +0700

    Change db prefix

--- internal/shardchain/dbfactory.go
+++ internal/shardchain/dbfactory.go
@@ -15,8 +15,8 @@ import (
 )
 
 const (
-	LDBDirPrefix      = "harmony_db"
-	LDBShardDirPrefix = "harmony_sharddb"
+	LDBDirPrefix      = "posichain_db"
+	LDBShardDirPrefix = "posichain_sharddb"
 )
 
 // DBFactory is a blockchain database factory.
