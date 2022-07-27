a6fa00589a33884f4ebf05d0bb7cc368946557d6
Danny <danny@position.exchange>
2022-07-27 16:18:29 +0700

    Update devnet deployment

--- .deployment/.docker/scripts/node_run.sh
+++ .deployment/.docker/scripts/node_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$ENABLE_REMOTE_DEBUG" == "true" ]]; then
-  dlv --listen=:40000 --headless=true --api-version=2 --accept-multiclient --log=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc exec ./bin/harmony -- --bls.pass.file=passphrases/passphrase.txt -c=./harmony.conf
+  dlv --listen=:40000 --headless=true --api-version=2 --accept-multiclient --log=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc exec ./bin/harmony -- --bls.pass.file=passphrases/passphrase.txt -c=./posichain.conf
 else
-  ./bin/harmony --bls.pass.file=passphrases/passphrase.txt -c=./harmony.conf
+  ./bin/harmony --bls.pass.file=passphrases/passphrase.txt -c=./posichain.conf
 fi
