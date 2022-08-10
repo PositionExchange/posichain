3a40f35e0f00af223a9b9a7298a94bf21a6b966d
Danny <danny@position.exchange>
2022-08-10 14:28:11 +0700

    Update devnet accounts

--- internal/genesis/devnet_account.go
+++ internal/genesis/devnet_account.go
@@ -2,16 +2,16 @@ package genesis
 
 // DevnetOperatedAccounts are the accounts for the initial genesis nodes hosted by Posichain for devnet.
 var DevnetOperatedAccounts = []DeployAccount{
-	{Index: "0", ShardID: 0, Address: "0x7F868BD9C67e3C83C6FEAB22ba029f69ed3E15d4", BLSPublicKey: "f47b6d2b91eb37a5c0b35520803b86901a086367506e423f1651464191cc95b67b95470b8bc41ea4ad57ead0739fc180"},
-	{Index: "1", ShardID: 1, Address: "0xDC9ed4a0FBaF250E080fa02B850D66763AdF7e37", BLSPublicKey: "8fe989abe5334b53f85419d3e41225d615e60a5b536d280415e125df4f7f4136040d434c20b67a2848c2c24e5b06f38f"},
-	{Index: "2", ShardID: 0, Address: "0xA616522dF16bc7aBb38C6CB99f2dA2919C48665C", BLSPublicKey: "25b597c52cfc071fd00b626844cb45c6e8a0252bd73841be21dbcb9623e136a3644d4a218dc5a6f76f7052e3569f0308"},
-	{Index: "3", ShardID: 1, Address: "0x8DCAD03eadf7039d44BE8b669D70Ce6c16Ef6f66", BLSPublicKey: "77c865f73d76fc4554b63f0a0d0e34a6a1272d02578044a6e14f2e55bbc6dde1ef7ac0046f81c21752f91bb823e8e819"},
+	{Index: "0", ShardID: 0, Address: "0x58AcF11BF43F2dDeE961C079ade3BAC9da83Dd0f", BLSPublicKey: "bad4b445ae3dbb130fc13f42d7f1065e7208fd05444a68a615768f4bedf6beb4ae2f0c4768810fb556cf2ecff49e330a"},
+	{Index: "1", ShardID: 1, Address: "0x72948DE451561eb58e36d24E45FF64e0D97Ae7B0", BLSPublicKey: "31ad8c6c7effad3af47ba8f31b18ae1b0168864e88b47b100cb69e1eae040976b356b1ed5f13771ef87c13a84198798d"},
+	{Index: "2", ShardID: 0, Address: "0xE247fd7041a714B047C40F0b06d1c83B9458Be11", BLSPublicKey: "de3bfaa629c0d7d1a167b930f040b4ca35a3ba72efe0dc8330ecf3bfceb1815d989871a13e41c04008715f97d62bb106"},
+	{Index: "3", ShardID: 1, Address: "0xa3A6238a7139eFd6a94E678f4bb0706Bd513163E", BLSPublicKey: "6e2a288108bfb8c4933c053b8f8a5e399301d9bdd455e2cca3916eaa56f5ec5401b2d8b0b9aa370a46720512bcb24e09"},
 }
 
 // DevnetFoundationalAccounts are the accounts for the initial foundational nodes for devnet.
 var DevnetFoundationalAccounts = []DeployAccount{
-	{Index: "0", ShardID: 0, Address: "0xC90c19Bb0498070136aB0dE7866592CDeE375Ceb", BLSPublicKey: "8274b0ad76ad77a1ebf19365cfe64564781ad988d276c1bd056f4b836e90dbd9bc30303432f2109721ede934ffbafb14"},
-	{Index: "1", ShardID: 1, Address: "0xFcF6664d04b339650B5acC7fb140758788899f75", BLSPublicKey: "8cb7325a2f2d9532a657a3404de9c5c5eaa2c45e1b1e4e72a2fe2051d48ec3525d7146cbf79b3f72f9faccffcc0da105"},
-	{Index: "2", ShardID: 0, Address: "0x4655b4ce5842c20127c5d0EA36FA1d3cb7786d13", BLSPublicKey: "683dee10231b0b40323373d903b799c2eaadd7b2bba5ce09b3bcddf8153bdd93edc40b8b75dbed762bfae4f16fdd4c8c"},
-	{Index: "3", ShardID: 1, Address: "0xb2eADEe811387055aFe38A61BeFe5a76ad612785", BLSPublicKey: "5f42f1ac23adf59dc6d535d0f422967cf79f5b95f2489de7a127c20e44b9d659e60eed2b19a7bf6fcb229b4f1018a60f"},
+	{Index: "0", ShardID: 0, Address: "0x1BAAC0973CE99D8048936D5b102Dd55B62047ea0", BLSPublicKey: "3c10d84c6b7f3aa0204ed00fdc9b9406f612ed46c8356054087b8bb3fda63cb6840f4bacaaca118f1166ddcbb3c54e06"},
+	{Index: "1", ShardID: 1, Address: "0x22bd5bbf88f7735DEF9378FD50D952BACD8c2339", BLSPublicKey: "210db574c3bcaf52da54fe0a09678502773d837d22d0200631fe32e860e2f4c7faa416ef7b4469e8c57da0e4a0edaa8b"},
+	{Index: "2", ShardID: 0, Address: "0x237bE5DB39613077e04402310fa6f844acc04F25", BLSPublicKey: "deb404802a80b002557b952a499ffc48b9cddaffca3522357aee2cedcbab3202dcd35109948a11b9296c592d6f641508"},
+	{Index: "3", ShardID: 1, Address: "0x8f943B9AF1c2189b734c317d83d791A479d65fCc", BLSPublicKey: "2db4e71fe73efd575b4279c7e0a9cf6b02ec2c3f0cdd9d1d645333c1e0191d26f93fd8a0aa38e968cfa78ca377cd5d8d"},
 }
